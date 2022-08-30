d01fd49bf935c93cafc9cfc35bdd6c74fda6fd09
jakabe <jacobcgwenger93@gmail.com>
2022-08-29 20:14:33 -0600

    massive changes

--- script.js
+++ script.js
@@ -1,172 +1,109 @@
-var scanlines = $('.scanlines');
-var tv = $('.tv');
-function exit() {
-    $('.tv').addClass('collapse');
-    term.disable();
-}
-
-// ref: https://stackoverflow.com/q/67322922/387194
-var __EVAL = (s) => eval(`void (__EVAL = ${__EVAL}); ${s}`);
-
-var term = $('#terminal').terminal(function(command, term) {
-    var cmd = $.terminal.parse_command(command);
-    if (cmd.name === 'exit') {
-        exit();
-    } else if (cmd.name === 'echo') {
-        term.echo(cmd.rest);
-    } else if (command !== '') {
-        try {
-            var result = __EVAL(command);
-            if (result && result instanceof $.fn.init) {
-                term.echo('<#jQuery>');
-            } else if (result && typeof result === 'object') {
-                tree(result);
-            } else if (result !== undefined) {
-                term.echo(new String(result));
-            }
-        } catch(e) {
-            term.error(new String(e));
-        }
-    }
-}, {
-    name: 'command_line',
-    onResize: set_size,
-    exit: false,
-    // detect iframe codepen preview
-    enabled: $('body').attr('onload') === undefined,
-    onInit: function() {
-        set_size();
-        this.echo('Type [[b;#fff;]exit] to see turn off animation.');
-        this.echo('Type and execute [[b;#fff;]grab()] function to get the scre' +
-                  'enshot from your camera');
-        this.echo('Type [[b;#fff;]camera()] to get video and [[b;#fff;]pause()]/[[b;#fff;]play()] to stop/play');
+var Typer = {
+    text: '',
+    accessCountimer: null,
+    index: 0,
+    speed: 2,
+    file: '',
+    accessCount: 0,
+    deniedCount: 0,
+    init: function () {
+      accessCountimer = setInterval(function () {
+        Typer.updLstChr();
+      }, 500);
+      $.get(Typer.file, function (data) {
+        Typer.text = data;
+        Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+      });
     },
-    onClear: function() {
-        console.log(this.find('video').length);
-        this.find('video').map(function() {
-            console.log(this.src);
-            return this.src;
-        });
+  
+    content: function () {
+      return $('#console').html();
     },
-    prompt: 'Runner:> '
-});
-// for codepen preview
-if (!term.enabled()) {
-    term.find('.cursor').addClass('blink');
-}
-function set_size() {
-    // for window height of 170 it should be 2s
-    var height = $(window).height();
-    var width = $(window).width()
-    var time = (height * 2) / 170;
-    scanlines[0].style.setProperty("--time", time);
-    tv[0].style.setProperty("--width", width);
-    tv[0].style.setProperty("--height", height);
-}
-
-function tree(obj) {
-    term.echo(treeify.asTree(obj, true, true));
-}
-var constraints = {
-    audio: false,
-    video: {
-        width: { ideal: 1280 },
-        height: { ideal: 1024 },
-        facingMode: "environment"
-    }
-};
-var acceptStream = (function() {
-    return 'srcObject' in document.createElement('video');
-})();
-function camera() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        term.pause();
-        var media = navigator.mediaDevices.getUserMedia(constraints);
-        media.then(function(mediaStream) {
-            term.resume();
-            var stream;
-            if (!acceptStream) {
-                stream = window.URL.createObjectURL(mediaStream);
-            } else {
-                stream = mediaStream;
-            }
-            term.echo('<video data-play="true" class="self"></video>', {
-                raw: true,
-                onClear: function() {
-                    if (!acceptStream) {
-                        URL.revokeObjectURL(stream);
-                    }
-                    mediaStream.getTracks().forEach(track => track.stop());
-                },
-                finalize: function(div) {
-                    var video = div.find('video');
-                    if (!video.length) {
-                        return;
-                    }
-                    if (acceptStream) {
-                        video[0].srcObject = stream;
-                    } else {
-                        video[0].src = stream;
-                    }
-                    if (video.data('play')) {
-                        video[0].play();
-                    }
-                }
-            });
-        });
-    }
-}
-var play = function() {
-    var video = term.find('video').slice(-1);
-    if (video.length) {
-        video[0].play();
-    }
-}
-function pause() {
-    term.find('video').each(function() {
-        this.pause(); 
-    });
-}
-
-function grab() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        term.pause();
-        var media = navigator.mediaDevices.getUserMedia(constraints);
-        media.then(function(mediaStream) {
-            const mediaStreamTrack = mediaStream.getVideoTracks()[0];
-            const imageCapture = new ImageCapture(mediaStreamTrack);
-            return imageCapture.takePhoto();
-        }).then(function(blob) {
-            term.echo('<img src="' + URL.createObjectURL(blob) + '" class="self"/>', {
-                raw: true,
-                finialize: function(div) {
-                    div.find('img').on('load', function() {
-                        URL.revokeObjectURL(this.src);
-                    });
-                }
-            }).resume();
-        }).catch(function(error) {
-            term.error('Device Media Error: ' + error);
-        });
-    }
-}
-async function pictuteInPicture() {
-    var [video] = $('video');
-    try {
-        if (video) {
-            if (video !== document.pictureInPictureElement) {
-                await video.requestPictureInPicture();
-            } else {
-                await document.exitPictureInPicture();
-            }
+  
+    write: function (str) {
+      $('#console').append(str);
+      return false;
+    },
+  
+    addText: function (key) {
+      if (key.keyCode == 18) {
+        Typer.accessCount++;
+  
+        if (Typer.accessCount >= 3) {
+          Typer.makeAccess();
         }
-  } catch(error) {
-      term.error(error);
+      } else if (key.keyCode == 20) {
+        Typer.deniedCount++;
+  
+        if (Typer.deniedCount >= 3) {
+          Typer.makeDenied();
+        }
+      } else if (key.keyCode == 27) {
+        Typer.hidepop();
+      } else if (Typer.text) {
+        var cont = Typer.content();
+        if (cont.substring(cont.length - 1, cont.length) == '|')
+          $('#console').html(
+            $('#console')
+              .html()
+              .substring(0, cont.length - 1),
+          );
+        if (key.keyCode != 8) {
+          Typer.index += Typer.speed;
+        } else {
+          if (Typer.index > 0) Typer.index -= Typer.speed;
+        }
+        var text = Typer.text.substring(0, Typer.index);
+        var rtn = new RegExp('\n', 'g');
+  
+        $('#console').html(text.replace(rtn, '<br/>'));
+        window.scrollBy(0, 50);
+      }
+  
+      if (key.preventDefault && key.keyCode != 122) {
+        key.preventDefault();
+      }
+  
+      if (key.keyCode != 122) {
+        // otherway prevent keys default behavior
+        key.returnValue = false;
+      }
+    },
+  
+    updLstChr: function () {
+      var cont = this.content();
+  
+      if (cont.substring(cont.length - 1, cont.length) == '|')
+        $('#console').html(
+          $('#console')
+            .html()
+            .substring(0, cont.length - 1),
+        );
+      else this.write('|'); // else write it
+    },
+  };
+  
+  function replaceUrls(text) {
+    var http = text.indexOf('http://');
+    var space = text.indexOf('.me ', http);
+  
+    if (space != -1) {
+      var url = text.slice(http, space - 1);
+      return text.replace(url, '<a href="' + url + '">' + url + '</a>');
+    } else {
+      return text;
+    }
   }
-}
-function clear() {
-    term.clear();
-}
-
-github('jcubic/jquery.terminal');
-cssVars(); // ponyfill
+  
+  Typer.speed = 3;
+  Typer.file = 'welcome_runner.txt';
+  Typer.init();
+  
+  var timer = setInterval('t();', 30);
+  function t() {
+    Typer.addText({ keyCode: 123748 });
+  
+    if (Typer.index > Typer.text.length) {
+      clearInterval(timer);
+    }
+  }
